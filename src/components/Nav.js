6f7ec273ba751eb8bd74e9091fded2c6b8ee4901
alpinelife37 <54160490+alpinelife37@users.noreply.github.com>
2020-03-17 12:18:52 -0700

    updated search.js

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -2,7 +2,7 @@ import React from "react";
 
 class NavBar extends React.Component {
   render() {
-    return <h1 className="cont">Employee Directory</h1>;
+    return <h1 className="cont">Employee Listing</h1>;
   }
 }
 
